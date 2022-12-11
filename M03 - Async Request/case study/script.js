edff3a4da402c555a48d8645eb0cfe9b9f86df50
Cindy Sintiya <120317466+cinnamondys@users.noreply.github.com>
2022-12-11 21:51:55 +0700

    Add files via upload

---
+++ M03 - Async Request/case study/script.js
@@ -0,0 +1,30 @@
+let show = function(data) {
+    let arr = JSON.parse(data)
+    arr = arr[0]
+    document.getElementById("gambar").src = arr.download_url
+    document.getElementById("author").innerHTML = arr.author
+}
+
+let load = function() {
+    let link = "https://picsum.photos/v2/list?page=2&limit=1"
+    let xhr
+    xhr = new XMLHttpRequest()
+
+    try {
+        xhr = new XMLHttpRequest()
+        if (!xhr) return -1
+        
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                show(xhr.responseText)
+            }
+        }
+        xhr.open('GET', link)
+        xhr.send()
+    }
+    catch(e) {
+        console.log(e)
+    }
+}
+
+load()
