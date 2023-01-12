802f1767652c26d192aba4b86e54a20c7c24c1e5
cin-namon-dy-s <120317466+cinnamondys@users.noreply.github.com>
2023-01-12 13:52:06 +0700

    Delete M13 - Basic Authentication directory

--- M13 - Basic Authentication/latihan/models/model.js
+++
@@ -1,8 +0,0 @@
-import {Sequelize, DataTypes} from 'sequelize'
-
-const sequelize = new Sequelize("store_db", "root", "jIa25052003", {  // db name, username, password
-    host    : 'localhost', 
-    dialect : 'mysql' 
-});
-
-export {sequelize, DataTypes}
