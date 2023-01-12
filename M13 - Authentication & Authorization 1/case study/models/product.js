c0faccfa331c79fc93a70313dcd9277c7088ac0f
cin-namon-dy-s <120317466+cinnamondys@users.noreply.github.com>
2023-01-12 13:42:31 +0700

    Delete M13 - Authentication & Authorization 1 directory

--- M13 - Authentication & Authorization 1/case study/models/product.js
+++
@@ -1,8 +0,0 @@
-import {sequelize, DataTypes} from "./model.js"
-
-const Product = sequelize.define('products', {
-    name  : DataTypes.STRING, 
-    price : DataTypes.INTEGER
-})
-
-export default Product
