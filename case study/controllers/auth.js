3b9b93688d1c2808f45b47ad6d655bddc79edddb
Cindy Sintiya <105113098+cindysintiya@users.noreply.github.com>
2023-01-29 12:08:43 +0700

    Add files via upload

---
+++ case study/controllers/auth.js
@@ -0,0 +1,9 @@
+const forAdmin = (req,res,next)=>{
+    let user = req.session.user || ""
+    if (user && user.isAdmin)
+        next()
+    else
+        res.redirect('/forbidden')
+    }
+
+export default forAdmin;
